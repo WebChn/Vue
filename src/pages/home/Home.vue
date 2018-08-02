3893d74674305ffd50e8c31e6430d2c4ffa588c4
WebChn <39829436+WebChn@users.noreply.github.com>
2018-08-02 19:47:25 +0800

    1

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -9,12 +9,7 @@
 <script>
 import HomeHeader from './components/Header' // 引入了局部组件
 import HomeSwiper from './components/Swiper' // 引入Swiper.vue组件
-<<<<<<< HEAD
-import HomeIcons from './components/Icons'
-
-=======
 import HomeIcons from './components/Icons'   // 引入了Icons.vue组件
->>>>>>> origin/index-icons
 export default {
   name: 'Home', 
   components: {
