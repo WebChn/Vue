614c85cb9be82a0103bdb2900424349c25c702ce
WebChn <39829436+WebChn@users.noreply.github.com>
2018-08-02 16:07:14 +0800

    add icon

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -9,9 +9,9 @@
 <script>
 import HomeHeader from './components/Header' // 引入了局部组件
 import HomeSwiper from './components/Swiper' // 引入Swiper.vue组件
-import HomeIcons from './components/Icons'
+import HomeIcons from './components/Icons'   // 引入了Icons.vue组件
 export default {
-  name: 'Home',
+  name: 'Home', 
   components: {
       HomeHeader: HomeHeader,     // 把局部组件注册到Home组件里 就可以在Home模板里使用
       HomeSwiper: HomeSwiper,   //es6语法 键和值相同 可以省略一部分
