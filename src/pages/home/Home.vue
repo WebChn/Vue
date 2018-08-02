2362ae80d292981084689d8625bd7135c61df36b
WebChn <39829436+WebChn@users.noreply.github.com>
2018-08-02 19:18:45 +0800

    add recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,24 +3,25 @@
         <home-header></home-header>
         <home-swiper></home-swiper>
         <home-icons></home-icons>
+        <home-recommend></home-recommend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header' // 引入了局部组件
 import HomeSwiper from './components/Swiper' // 引入Swiper.vue组件
-<<<<<<< HEAD
 import HomeIcons from './components/Icons'
-
-=======
-import HomeIcons from './components/Icons'   // 引入了Icons.vue组件
->>>>>>> origin/index-icons
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend' 
 export default {
   name: 'Home', 
   components: {
       HomeHeader: HomeHeader,     // 把局部组件注册到Home组件里 就可以在Home模板里使用
       HomeSwiper: HomeSwiper,   //es6语法 键和值相同 可以省略一部分
-      HomeIcons    
+      HomeIcons,    
+      HomeRecommend,
+      HomeWeekend
   }
 }
 </script>
