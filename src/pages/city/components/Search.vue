fdd3f343d2a6356d50174ee3fe38e28a0a393d2b
WebChn <39829436+WebChn@users.noreply.github.com>
2018-08-03 20:27:54 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,75 @@
 <template>
-   <div class="search">
-       <input class="search-input" type="text" placeholder="输入城市名或拼音">
-   </div>
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+        </div>
+        <div 
+            class="search-content" 
+            ref="search"
+            v-show="keyword"
+        >     <!--上面v-show部分 当输入框为空时 不生成区域 即原来的东西会正常显示 -->
+            <ul>
+                <li 
+                    class="search-item border-bottom" 
+                    v-for="item of list" 
+                    :key="item.id"
+                >
+                    {{item.name}}
+                </li>
+                <li class="search-item border-bottom" v-show="hasNoData"><!--如果list没有长度就出现 模板里尽量简洁 不要出现逻辑运算 逻辑运算在JS部分实现 -->
+                    没有找到匹配的城市
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    props: {
+        cities: Object
+    },
+    data () {
+        return {
+            keyword: '',
+            list: [],
+            timer: null,  // 做节流
+        }
+    },
+    computed: {
+        hasNoData () {
+            return !this.lislength
+        }
+    },
+    watch: {
+        keyword () {
+            if (this.timer) {
+                clearTimeout(this.timer) 
+            }
+            if (!this.keyword) {     //当输入值为空时 下面的可能项消失
+                    this.list = []
+                    return
+            }
+            this.timer = setTimeout(() => {
+                const result = []
+                
+                for(let i in this.cities) {
+                    this.cities[i].forEach((value) => {
+                        if (value.spell.indexOf(this.keyword) > -1 ||
+                        value.name.indexOf(this.keyword) > -1) {
+                            result.push(value)
+                        }
+                    })
+                }
+                this.list = result
+            }, 100)
+        }
+    },
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.search)
+    }
 }
 </script>
 
@@ -25,4 +88,19 @@ export default {
             text-align center
             border-radius .06rem
             color #666
+    .search-content
+        z-index 1
+        overflow hidden
+        position absolute 
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        background #eee 
+        .search-item
+            line-height .62rem
+            padding-left .2rem
+            background #fff
+            color #666
+            border-bottom .1px #eee solid
 </style>
